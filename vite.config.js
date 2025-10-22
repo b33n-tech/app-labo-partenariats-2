0bdc05badfdd821c1e7b999eb536190049b95945
b33n-tech <benjamin.griot@outlook.fr>
2025-10-22 20:10:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚠️ Important : remplacer 'animateur-webapp' par le nom exact de ton repo GitHub
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/animateur-webapp/'
 })
